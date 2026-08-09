994ba3d00c4d3bf84bcedf0bde3cee595a8cb873
t-ooshiro0125 <corgi.shim0125@gmail.com>
2026-08-09 02:44:26 +0000

    ci: GitHub Pagesへのデプロイと品質チェックを設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://t-ooshiro0125.github.io",
+  base: "/working-corgi",
+});
