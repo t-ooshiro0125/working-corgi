09e5a60d7c68fb695b035ef48b6327ef6b6f99a6
t-ooshiro0125 <47342900+t-ooshiro0125@users.noreply.github.com>
2026-08-12 07:59:27 +0900

    feat: 独自ドメインとXリンクを設定 (#17)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://t-ooshiro0125.github.io",
-  base: "/working-corgi",
+  site: "https://workingcorgi.com",
 });
