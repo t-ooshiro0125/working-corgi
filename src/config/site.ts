5dd837854340b56ba01e4092330c6cd3391eb805
t-ooshiro0125 <corgi.shim0125@gmail.com>
2026-08-16 23:43:18 +0000

    feat: Contact ページを追加

---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const profiles = {
+  github: {
+    url: "https://github.com/t-ooshiro0125",
+    handle: "t-ooshiro0125",
+  },
+  x: {
+    url: "https://x.com/working_corgi",
+    handle: "@working_corgi",
+  },
+} as const;
